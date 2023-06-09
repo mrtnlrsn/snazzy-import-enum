f7d77bd9ff4f0c853c888b3c7b6a622782764904
Morten Laursen <mla@mydefence.dk>
2023-06-09 13:47:38 +0200

    Move enum Size to common.proto
    Causes build error.
    
    Change-Id: I167c5087d9731ff18e35b311135a9dff5a2eb76e

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/items.proto"],
-                                &["src/"]).unwrap();
+    prost_build::compile_protos(&["src/common.proto", "src/items.proto"], &["src/"]).unwrap();
 }
