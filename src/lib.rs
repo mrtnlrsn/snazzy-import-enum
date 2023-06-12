55b9a56bdd62022e3d31e2d6368526d20cbaf85a
Morten Laursen <mla@mydefence.dk>
2023-06-12 12:53:43 +0200

    issue fixed
    Change-Id: I0727e9cc5dfaad5aa2bc2c5b500c811539d7c2f0

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ use std::io::Cursor;
 use prost::Message;
 
 // Include the `items` module, which is generated from items.proto.
+pub mod common {
+    include!(concat!(env!("OUT_DIR"), "/snazzy.common.rs"));
+}
 pub mod items {
     include!(concat!(env!("OUT_DIR"), "/snazzy.items.rs"));
 }
@@ -10,7 +13,7 @@ pub mod items {
 pub fn create_large_shirt(color: String) -> items::Shirt {
     let mut shirt = items::Shirt::default();
     shirt.color = color;
-    shirt.set_size(items::shirt::Size::Large);
+    shirt.set_size(common::Size::Large);
     shirt
 }
 
